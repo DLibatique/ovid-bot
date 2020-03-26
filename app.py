65b619338d017ceaa50d926bfd26f4087bd7894a
Justin Chiou <chiou.kai@gmail.com>
2020-03-26 19:26:54 -0400

    add app back in

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from random import choice
-# from .twee import api
+from twee import api
 from parser import clean_met
 
 
@@ -9,6 +9,6 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', seconds=5)
 def tweet_line():
     print(choice(clean_met))
-    # api.update_status(choice(clean_met))
+    api.update_status(choice(clean_met))
 
 sched.start()
